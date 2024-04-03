b7a7bf356062227929d7a674dbb81b0b5add1a0b
ilya <ilya.biryuk2005@icloud.com>
2024-04-03 14:14:58 +0300

    move

--- Matrix/Main.cpp
+++ Matrix/Main.cpp
@@ -1,6 +1,15 @@
 //Biryuk Ilya, 11 Group
+#include "Matrix.h"
 
 int main() {
-
+	{
+		SquareMatrix<8> m;
+	}
+	{
+		//Matrix<8,9> m;
+		//but
+		//Matrix<8,8> m; //too
+		//Matrix<8> m; //not
+	}
 	return 0;
 }
--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <vector>
 #include "Rational.h"
 
@@ -10,9 +11,7 @@ public:
 		return false;
 	}
 	template <>
-	bool operator==<N, M, Field>(Matrix<Q, I, F> rhs) const;
-	template <size_t Q, typename F = Field>
-	Matrix<Q, Q, F>(); //E //!think about forbid non-sqare
+	bool operator==<N, M, Field>(Matrix<N, M, Field> rhs) const;
 	Matrix<N, M, Field>(std::vector<std::vector<Field>> v); //you have to enter size
 	Matrix<N, M, Field> operator+(Matrix<N, M, Field> rhs) const;
 	Matrix<N, M, Field> operator-(Matrix<N, M, Field> rhs) const;
@@ -26,7 +25,79 @@ public:
 	template <size_t Q, typename F = Field>
 	Matrix<Q, Q, Field>& invert();
 	//trace
-protected:
+	std::vector<Field> getRow(size_t i) const;
+	std::vector<Field> getColumn(size_t i) const;
+	std::vector<Field>& operator[](size_t Row);
+	const std::vector<Field>& operator[](size_t Row) const;
 	template <size_t Q, size_t I, typename F>
-	Matrix<Q, I, F>();
+	friend std::ostream& operator<<(std::ostream& os, Matrix<Q, I, F> obj);
+protected:
+	Matrix(); //Only squareMatrix
+	std::vector<std::vector<Field>> v;
 };
+
+template <size_t N, typename Field = Rational>
+class Matrix : Matrix<N, N, Field> {
+
+};
+
+template <size_t N, size_t M, typename Field>
+Matrix<N, M, Field>::Matrix() {
+
+}
+
+template<size_t N, size_t M, typename Field>
+Matrix<N, M, Field>::Matrix(std::vector<std::vector<Field>> v) : v(v)
+{
+
+}
+
+template<size_t N, size_t M, typename Field>
+inline Matrix<N, M, Field>& Matrix<N, M, Field>::operator+=(Matrix<N, M, Field> rhs)
+{
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < M; j++)
+		{
+			v[i][j] += rhs.v[i][j];
+		}
+	}
+	return *this;
+}
+
+template<size_t N, size_t M, typename Field>
+inline Matrix<N, M, Field> Matrix<N, M, Field>::operator-(Matrix<N, M, Field> rhs) const
+{
+	for (size_t i = 0; i < N; i++)
+	{
+		for (size_t j = 0; j < M; j++)
+		{
+			v[i][j] -= rhs.v[i][j];
+		}
+	}
+	return *this;
+}
+
+template<size_t N, size_t M, typename Field>
+inline Matrix<N, M, Field>& Matrix<N, M, Field>::operator-=(Matrix<N, M, Field> rhs)
+{
+	Matrix r(*this);
+	return (r -= rhs);
+}
+
+template<size_t N, size_t M, typename Field>
+Matrix<N, M, Field> Matrix<N, M, Field>::operator+(Matrix<N, M, Field> rhs) const
+{
+	Matrix r(*this);
+	return (r += rhs);
+}
+
+template <size_t N, typename Field = Rational>
+struct SquareMatrix : Matrix<N, N, Field> {
+	SquareMatrix();
+};
+
+template <size_t N, typename Field>
+SquareMatrix<N, Field>::SquareMatrix() : Matrix<N, N, Field>() {
+
+}
