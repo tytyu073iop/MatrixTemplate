39dff19765c8e071a1acb5f0ed700d18d4e4045b
tytyu073iop <iiya.ps4.com@gmail.com>
2024-04-05 21:59:23 +0300

    save example

--- Matrix/Main.cpp
+++ Matrix/Main.cpp
@@ -3,13 +3,27 @@
 
 int main() {
 	{
-		SquareMatrix<8> m;
+		Matrix<8, 8, int> m;
+		m.det();
+		//m + m;
 	}
 	{
-		//Matrix<8,9> m;
+		SquareMatrix<8> q;
+		SquareMatrix<8, int> m;
+		m.det();
+	}
+	{
+		//Matrix<8,9> m; //no construct
 		//but
 		//Matrix<8,8> m; //too
 		//Matrix<8> m; //not
 	}
+	
+	// {
+	// 	std::cout << "Enter the size of the matrix: ";
+	// 	int n;
+	// 	std::cin >> n;
+	// 	Matrix<n, n> m;
+	// } //do not work
 	return 0;
 }
